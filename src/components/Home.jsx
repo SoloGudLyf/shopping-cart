4c70c5245e5a0801b779063699bd9ffb3cb44e88
SoloGudLyf <solomonteiko0@gmail.com>
2025-12-01 11:36:10 +0000

    feat: Import styles and apply to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
 import {Link} from "react-router"
+import styles from "../styles/Home.module.css"
 
 export function Homepage(){
-    return <div className="container">
-        <div className="navBar"><span>Logo</span><Link to="/">Home</Link><Link>Shop</Link>
-        <Link>Cart</Link></div>
-        <div className="content"><h1>Welcome to my shopping page</h1></div>
-        <div className="footer"></div>
+    return <div className={styles.container}>
+        <div className={styles.navBar}><span>Logo</span><span>
+            <Link to="/">Home</Link><Link>Shop</Link>
+            <Link>Cart</Link>
+        </span></div>
+        <div className={styles.content}><h1>Welcome to my shopping page</h1></div>
+        <div className={styles.footer}></div>
     </div>
 }
