c4280757b8d45c8e3bba04d751b5b600b4cf7538
SoloGudLyf <solomonteiko0@gmail.com>
2025-12-02 17:30:44 +0000

    feat: Implement Image carousel logic
    To showcase wares

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,42 @@
-import {Link} from "react-router"
-import styles from "../styles/Home.module.css"
+import { Link } from "react-router";
+import styles from "../styles/Home.module.css";
+import { ImageCarousel } from "./image-carousel/image-carousel/ImageCarousel";
 
-export function Homepage(){
-    return <div className={styles.container}>
-        <div className={styles.navBar}><span>Logo</span><span>
-            <Link to="/">Home</Link><Link>Shop</Link>
-            <Link>Cart</Link>
-        </span></div>
-        <div className={styles.content}><h1>Welcome to my shopping page</h1></div>
-        <div className={styles.footer}></div>
+export function Homepage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.navBar}>
+        <span>Logo</span>
+        <span>
+          <Link to="/">Home</Link>
+          <Link>Shop</Link>
+          <Link>Cart</Link>
+        </span>
+      </div>
+      <div className={styles.content}>
+        <h1>Welcome to my shopping page</h1>
+        <ImageCarousel
+          imagesArr={[
+            {
+              src: "src/components/image-carousel/image-carousel/images/cli-webpack.png",
+              alt: "Screenshot",
+            },
+            {
+              src: "src/components/image-carousel/image-carousel/images/Screenshot_2025-10-13_13-02-48.png",
+              alt: "Screenshot",
+            },
+            {
+              src: "src/components/image-carousel/image-carousel/images/Screenshot_2025-11-26_01-28-21.png",
+              alt: "Screenshot",
+            },
+            {
+              src: "src/components/image-carousel/image-carousel/images/vscode-webpack.png",
+              alt: "screenshot",
+            },
+          ]}
+        />
+      </div>
+      <div className={styles.footer}></div>
     </div>
+  );
 }
---
+++ src/components/image-carousel/image-carousel/ImageCarousel.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import styles from "/home/gud-lyf/repos/shopping-cart/src/styles/ImageCarousel.module.css";
+
+export function ImageCarousel({ imagesArr }) {
+  const [currIndex, setCurrIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrIndex((prevIndex) => {
+        const nextIndex = prevIndex + 1;
+
+        if (nextIndex > imagesArr.length - 1) {
+          return 0;
+        }
+
+        return nextIndex;
+      });
+    }, 5000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  function moveBack() {
+    if (currIndex === 0) {
+      setCurrIndex(imagesArr.length - 1);
+      return;
+    } else if (currIndex <= imagesArr.length - 1) {
+      setCurrIndex(currIndex - 1);
+    }
+  }
+
+  function moveForward() {
+    if (currIndex >= imagesArr.length - 1) {
+      setCurrIndex(0);
+
+      return;
+    } else if (currIndex < imagesArr.length - 1) {
+      setCurrIndex(currIndex + 1);
+
+      return;
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <div className={styles.display}>
+          <div className={styles.triangleLeft} onClick={moveBack}></div>
+          <div className={styles.pictureFrame}>
+            {imagesArr.map((image, index) => (
+              <img
+                key={index}
+                className={
+                  index === currIndex ? styles.fadeIn : styles.inactive
+                }
+                src={index == currIndex ? image.src : "#"}
+                alt={image.alt}
+              />
+            ))}
+          </div>
+          <div className={styles.triangleRight} onClick={moveForward}></div>
+        </div>
+        <div className={styles.imageNav}>
+          {imagesArr.map((element, index) => {
+            return (
+              <input
+                key={index}
+                type="radio"
+                id={index}
+                name="image"
+                checked={index == currIndex}
+                onChange={() => setCurrIndex(index)}
+              />
+            );
+          })}
+        </div>{" "}
+      </div>
+    </div>
+  );
+}
