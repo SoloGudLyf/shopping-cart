bbd3f093d021a9100116be92e6eb1a2c1f2d57a4
SoloGudLyf <solomonteiko0@gmail.com>
2025-12-03 18:20:56 +0000

    feat: Test image carousel render logic
    Make sure an image is rendered when image carousel loads

--- src/tests/imageCarousel.test.jsx
+++ src/tests/imageCarousel.test.jsx
@@ -1,10 +1,57 @@
-import { describe, it, expect } from 'vitest';
-describe('something truthy and falsy', () => {
-    it('true to be true', () => {
-      expect(true).toBe(true);
-    });
-  
-    it('false to be false', () => {
-      expect(false).toBe(false);
-    });
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { ImageCarousel } from "../components/image-carousel/image-carousel/ImageCarousel";
+describe("renders image carousel", () => {
+  it("includes an image", () => {
+    render(
+      <ImageCarousel
+        imagesArr={[
+          {
+            src: "src/components/image-carousel/image-carousel/images/rebar.png",
+            alt: "Screenshot",
+          },
+          {
+            src: "src/components/image-carousel/image-carousel/images/timber.png",
+            alt: "Screenshot",
+          },
+          {
+            src: "src/components/image-carousel/image-carousel/images/masonry.png",
+            alt: "Screenshot",
+          },
+          {
+            src: "src/components/image-carousel/image-carousel/images/roofing.png",
+            alt: "screenshot",
+          },
+        ]}
+      />
+    );
+    screen.debug();
+    expect(screen.getAllByRole("img").length).toBe(4);
   });
+
+  it("false to be false", () => {
+    render(
+      <ImageCarousel
+        imagesArr={[
+          {
+            src: "src/components/image-carousel/image-carousel/images/rebar.png",
+            alt: "Screenshot",
+          },
+          {
+            src: "src/components/image-carousel/image-carousel/images/timber.png",
+            alt: "Screenshot",
+          },
+          {
+            src: "src/components/image-carousel/image-carousel/images/masonry.png",
+            alt: "Screenshot",
+          },
+          {
+            src: "src/components/image-carousel/image-carousel/images/roofing.png",
+            alt: "screenshot",
+          },
+        ]}
+      />
+    );
+    expect(false).toBe(false);
+  });
+});
