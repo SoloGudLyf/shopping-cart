5727373fc37c6a168c26f570809db25fe35a38e5
SoloGudLyf <solomonteiko0@gmail.com>
2025-12-04 15:59:07 +0000

    feat: Setup Cart component with content to render

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,3 +1,46 @@
-export function CartPage(){
-    return
+
+
+export function CartPage({inventory}){
+    return  <div className="container">
+        <section className="">
+        <span>Logo</span>
+        <span>
+          <Link to="/">Home</Link>
+          <Link to="shop">Shop</Link>
+          <Link>Cart</Link>
+        </span>
+          </section>
+          <section>
+            <h1>Cart Page</h1>
+            <table>
+  <caption>
+Your Cart Summary  </caption>
+  <thead>
+    <tr>
+      <th scope="col">ITEM</th>
+      <th scope="col">PRICE</th>
+      <th scope="col">QUANTITY</th>
+      <th scope="col">TOTAL</th>
+    </tr>
+  </thead>
+  <tbody>
+   {inventory.map((item)=>{
+     <tr>
+     <th scope="row">{item.name}</th>
+     <td>{item.price}</td>
+     <td>{item.quantity}</td>
+     <td>{item.price*item.quantity}</td>
+   </tr>
+   })}
+  </tbody>
+  <tfoot>
+    <tr>
+      <th scope="row" colspan="3">Total Amount</th>
+      <td>23</td>
+    </tr>
+  </tfoot>
+</table>
+
+          </section>
+    </div>
 }
