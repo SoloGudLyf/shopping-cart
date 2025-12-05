806d4d4e406f5381c19592065f930c41b48a1fcc
SoloGudLyf <solomonteiko0@gmail.com>
2025-12-05 02:49:13 +0000

    feat: Test rendering of page elements

---
+++ src/tests/Cart.test.jsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { MemoryRouter } from "react-router";
+import userEvent from "@testing-library/user-event";
+import { CartPage } from "../components/cart/Cart";
+import { StateProvider } from "../components/sharedCart";
+
+describe("renders as expected", () => {
+  it("displays links", () => {
+    render(
+      <MemoryRouter>
+<StateProvider><CartPage /></StateProvider>        
+      </MemoryRouter>
+    );
+    expect(screen.getAllByRole("link").length).toBeGreaterThan(2);
+  });
+
+  it("Displays heading", () => {
+    render(
+      <MemoryRouter>
+<StateProvider><CartPage /></StateProvider>        
+      </MemoryRouter>
+    );
+    expect(screen.getByText(/cart page/i)).toBeInTheDocument();
+  });
+
+  it("Displays table", () => {
+    render(
+      <MemoryRouter>
+<StateProvider><CartPage /></StateProvider>        
+      </MemoryRouter>
+    );
+    expect(screen.getByRole("table")).toBeInTheDocument();
+  });
+});
--- src/tests/Shop.test.jsx
+++ src/tests/Shop.test.jsx
@@ -2,13 +2,14 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import Shop from "../components/shop/Shop";
 import { MemoryRouter } from "react-router";
+import { StateProvider } from "../components/sharedCart";
 
 describe("renders page correctly", () => {
   it("renders heading", () => {
     render(
       <MemoryRouter>
         {" "}
-        <Shop />
+       <StateProvider><Shop /></StateProvider> 
       </MemoryRouter>
     );
     console.log(screen.getByRole("heading", { name: /shopping/i }));
@@ -21,7 +22,7 @@ describe("renders page correctly", () => {
   it("renders navigation links", () => {
     render(
       <MemoryRouter>
-        <Shop />
+       <StateProvider><Shop /></StateProvider> 
       </MemoryRouter>
     );
     expect(screen.getAllByRole("link").length).toBeGreaterThan(2);
@@ -30,7 +31,7 @@ describe("renders page correctly", () => {
   it("renders cards", () => {
     render(
       <MemoryRouter>
-        <Shop />
+       <StateProvider><Shop /></StateProvider> 
       </MemoryRouter>
     );
     expect(screen.getAllByRole("card").length).toBeGreaterThan(2);
